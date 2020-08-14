2782c977d27b9f6911aa3d8751c42a94431b7e16
arayik0530 <arayik0530@gmail.com>
2020-08-14 17:01:57 +0400

    texture atlas created

--- desktop/src/com/rockbite/bootcamp/desktop/DesktopLauncher.java
+++ desktop/src/com/rockbite/bootcamp/desktop/DesktopLauncher.java
@@ -2,13 +2,31 @@ package com.rockbite.bootcamp.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 import com.rockbite.bootcamp.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 1200;
-		config.height = 800;
-		new LwjglApplication(new MyGdxGame(), config);
-	}
+    private static boolean rebuildAtlas = true;
+    private static boolean drawDebugOutline = true; //TODO
+
+    public static void main(String[] arg) {
+
+        if (rebuildAtlas) {
+            TexturePacker.Settings settings = new TexturePacker.Settings();
+            settings.maxWidth = 2048;
+            settings.maxHeight = 2048;
+            settings.duplicatePadding = false;
+            settings.debug = drawDebugOutline;
+            //TexturePacker.process(settings, source folder this can be in
+            TexturePacker.process(settings, "pics",
+                    "images",
+                    "butterflyAtlas");
+        }
+
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Butterfly";
+        config.width = 1200;
+        config.height = 800;
+        new LwjglApplication(new MyGdxGame(), config);
+    }
 }
