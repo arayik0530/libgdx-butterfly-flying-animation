bb275aaeadc35f1080125ead6e38e890d7c56a1d
arayik0530 <arayik0530@gmail.com>
2020-08-14 15:10:11 +0400

    initial commit

---
+++ core/src/com/rockbite/bootcamp/MyGdxGame.java
@@ -0,0 +1,54 @@
+package com.rockbite.bootcamp;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+
+	float xPos;
+	float yPos;
+
+	private final float delta = 0.2f;
+	private float counter;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.529f, 0.808f, 0.922f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		counter += Gdx.graphics.getDeltaTime();
+
+		if(counter >= 0.016){
+			animate();
+			counter = 0;
+		}
+
+		batch.begin();
+		batch.draw(img, xPos, yPos, 150, 200);
+		batch.end();
+//		System.out.println(Gdx.graphics.getDeltaTime());
+	}
+
+	private void animate() {
+		xPos += delta;
+		yPos += delta;
+	}
+
+	@Override
+	public void dispose () {
+//		System.out.println("Game Over");
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/rockbite/bootcamp/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.rockbite.bootcamp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rockbite.bootcamp.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 800;
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
